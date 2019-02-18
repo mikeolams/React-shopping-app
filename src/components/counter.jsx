a30d6b76cb4879e362bc5b046ea22871e0f93427
GMike the GD <mikeolams@yahoo.com>
2019-02-18 22:32:56 +0100

    add class m-2 to span element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,7 +7,7 @@ class Counter extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<span className="badge badge-primary">{this.formatCount()}</span>
+				<span className="badge badge-primary m-2">{this.formatCount()}</span>
 				<button>Increment</button>
 			</React.Fragment>
 			);
