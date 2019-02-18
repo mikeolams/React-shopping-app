ba530083bff7f1b742bb638da33a98ab6115333c
GMike the GD <mikeolams@yahoo.com>
2019-02-18 22:25:02 +0100

    remove state dynamic image

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,11 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
 	state ={
-		count: 0,
-		imageUrl: "https://picsum.photos/200"
+		count: 0
 	};
 	render() {
 		return (
 			<React.Fragment>
-				<img src={this.state.imageUrl} alt=""/>
 				<span>{this.formatCount()}</span>
 				<button>Increment</button>
 			</React.Fragment>
