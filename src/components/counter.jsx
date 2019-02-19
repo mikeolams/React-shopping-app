315494b479132200c8dfa92b1ccb9c7f77c75289
GMike the GD <mikeolams@yahoo.com>
2019-02-19 16:42:15 +0100

    remove inline styling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,7 @@ class Counter extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<span style={{fontSize: 3}} className="badge badge-primary m-2">{this.formatCount()}</span>
+				<span className="badge badge-primary m-2">{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 			</React.Fragment>
 			);
