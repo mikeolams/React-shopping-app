237a6fc07e0d5b43395d95853b6c4fcca3b11f06
GMike the GD <mikeolams@yahoo.com>
2019-02-19 17:44:01 +0100

    add key attribute to each tag

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 				<ul>
-					{this.state.tags.map(tag => <li>{ tag }</li>)}
+					{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
 				</ul>
 			</React.Fragment>
 			);
