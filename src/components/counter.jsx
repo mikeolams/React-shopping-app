5c5f3f8fdfd5be87446e5706deaa1e9120366474
GMike the GD <mikeolams@yahoo.com>
2019-02-19 21:03:03 +0100

    implement a handle method on the button element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,12 +12,19 @@ class Counter extends Component {
 		return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
 	}
 
+	handleIncrement(){
+		console.log('Increment Clicked');
+	}
+
 	render() {
 
 		return (
 			<React.Fragment>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
+				<button 
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm">Increment
+				</button>
 				<div>
 					{this.state.tags.length === 0 && 'Please create a new tag!'}
 				   	{this.renderTags()}	
