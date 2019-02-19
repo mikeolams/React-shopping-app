c4a1f42f3c335a55c7435f0624d3edae7e93501a
GMike the GD <mikeolams@yahoo.com>
2019-02-19 16:58:29 +0100

    change hard coded classes of span to dynamic

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,7 @@ class Counter extends Component {
 		classes += (this.state.count === 0) ?'warning' : 'primary';
 		return (
 			<React.Fragment>
-				<span className="badge badge-primary m-2">{this.formatCount()}</span>
+				<span className={classes}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 			</React.Fragment>
 			);
