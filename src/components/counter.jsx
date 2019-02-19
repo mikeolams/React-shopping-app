b4bd84ea2006e9bf0fcebd2ff110e65e8f8a2c30
GMike the GD <mikeolams@yahoo.com>
2019-02-19 16:48:51 +0100

    setting dynamic classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,8 @@ class Counter extends Component {
 	};
 
 	render() {
+		let classes = 'badge m-2 ';
+		classes += (this.state.count === 0) ?'badge-warning' : badge-primary;
 		return (
 			<React.Fragment>
 				<span className="badge badge-primary m-2">{this.formatCount()}</span>
