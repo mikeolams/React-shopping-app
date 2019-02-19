3d0693d7508c361c612bc90f40f32058ee2cc5e3
GMike the GD <mikeolams@yahoo.com>
2019-02-19 23:06:20 +0100

    remove tag implementation

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,14 @@ import React, {Component} from 'react';
 
 class Counter extends Component {
 	state ={
-		count: 0,
-		tags: ['tag1', 'tag2', 'tag3']
+		count: 0
 	};
 
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+	// renderTags() {
+	// 	if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-		return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
-	}
+	// 	return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>
+	// }
 
 	// constructor(){
 	// 	super();
@@ -37,10 +36,6 @@ class Counter extends Component {
 					onClick={(product) => this.handleIncrement(product)}
 					className="btn btn-secondary btn-sm">Increment
 				</button>
-				<div>
-					{this.state.tags.length === 0 && 'Please create a new tag!'}
-				   	{this.renderTags()}	
-				</div>
 			</React.Fragment>
 			);
 	}
