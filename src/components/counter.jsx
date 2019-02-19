d7e07c547983bea0695aa9c58e930d9810bbaf15
GMike the GD <mikeolams@yahoo.com>
2019-02-19 22:11:30 +0100

    implement doHandleIncrement to pass argument to another method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,10 +17,15 @@ class Counter extends Component {
 	// 	this.handleIncrement = this.handleIncrement.bind(this);
 	// }
 
-	handleIncrement = () => {
+	handleIncrement = (product) => {
 		// console.log('Increment Clicked', this);
 		// this.state.count++;
+		console.log(product);
 		this.setState({count: this.state.count +1})
+	}
+
+	doHandleIncrement = () => {
+		this.handleIncrement({id: 1});
 	}
 
 	render() {
@@ -29,7 +34,7 @@ class Counter extends Component {
 			<React.Fragment>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button 
-					onClick={this.handleIncrement}
+					onClick={this.doHandleIncrement}
 					className="btn btn-secondary btn-sm">Increment
 				</button>
 				<div>
